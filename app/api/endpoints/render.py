f9f3bde87e8b8fce41b35c7f615a79ee5796833d
Vishruth Devan <vishruthdevan@gmail.com>
2022-07-21 22:21:09 +0530

    feat: render and return the video on request

--- app/api/endpoints/render.py
+++ app/api/endpoints/render.py
@@ -1,8 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import FileResponse, StreamingResponse
+from schemas.video import Video
+from utils.render import render_video
 
 router = APIRouter()
 
 
-@router.get("/render")
-def render():
-    return {"message": "rendered video"}
+@router.post("/render")
+async def render(video: Video):
+    video_path = render_video(video)
+
+    def iterfile():
+        with open(video_path, mode="rb") as file_like:
+            yield from file_like
+
+    return StreamingResponse(iterfile(), media_type="video/mp4")
+
+    # return FileResponse(video_path)
