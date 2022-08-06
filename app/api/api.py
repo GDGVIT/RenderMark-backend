1865976a1a4aba47631c47990e4b5346e76dc432
Vishruth Devan <vishruthdevan@gmail.com>
2022-08-06 23:16:10 +0530

    feat: add endpoint to convert image to a link

--- app/api/api.py
+++ app/api/api.py
@@ -1,5 +1,6 @@
-from api.endpoints import render
+from api.endpoints import render, upload
 from fastapi import APIRouter
 
 api_router = APIRouter()
 api_router.include_router(render.router, tags=["render"])
+api_router.include_router(upload.router, tags=["image-to-link"])
