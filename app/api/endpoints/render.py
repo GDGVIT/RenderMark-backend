477027e745e1c6e6038bc99c11295eeb20fe82af
Vishruth Devan <vishruthdevan@gmail.com>
2022-08-07 18:26:16 +0530

    feat: create endpoints to check status of background jobs

--- app/api/endpoints/render.py
+++ app/api/endpoints/render.py
@@ -1,19 +1,44 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import FileResponse, StreamingResponse
+from urllib.error import HTTPError
+from uuid import uuid4
+
+from celery.result import AsyncResult
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse, JSONResponse, StreamingResponse
 from schemas.video import Video
+from tasks.tasks import render_video_task
 from utils.render import render_video
+from worker import celery
 
 router = APIRouter()
 
 
 @router.post("/render")
 async def render(video: Video):
-    video_path = render_video(video)
+    task = render_video_task.delay(video.dict())
+    return JSONResponse({"task_id": task.id})
 
-    def iterfile():
-        with open(video_path, mode="rb") as file_like:
-            yield from file_like
+    # def iterfile():
+    #     with open(video_path, mode="rb") as file_like:
+    #         yield from file_like
 
-    return StreamingResponse(iterfile(), media_type="video/mp4")
+    # return StreamingResponse(iterfile(), media_type="video/mp4")
 
     # return FileResponse(video_path)
+
+
+@router.get("/status/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result,
+    }
+    return JSONResponse(result)
+
+
+@router.get("/videos/{video_path}")
+def get_video(video_path):
+    if video_path:
+        return FileResponse(f"media/{video_path}")
+    raise HTTPException(status_code=404, detail="Video not found")
